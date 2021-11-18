0c43d4088489eb4e8642231601f334df1c5bab75
Rafael Contreras <raf@contrer.as>
2021-11-19 11:14:07 +1300

    fix(CI): 💚  Add missing `useState`

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { Trans, useI18next } from "gatsby-plugin-react-i18next";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
